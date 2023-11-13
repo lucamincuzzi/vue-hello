4c6c939b5ac0c88e4d2cf572d0768c532ea736aa
lucamincuzzi <luca.mincuzzi@outlook.it>
2023-11-13 16:06:28 +0100

    add: added image via data fn

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-            title: "Questo è il mio progetto con Vue!"
+            title: "Questo è il mio primo progetto con Vue!",
+            image: "https://plus.unsplash.com/premium_photo-1675827055694-010aef2cf08f?q=80&w=2024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         }
     }
 });
